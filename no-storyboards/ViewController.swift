8a3b03bae25e6256ac61c895466626a7b0290e44
Manny <Emanuel@emanuels-macbook-pro.local>
2016-02-29 15:14:13 -0500

    Initial Commit

---
+++ no-storyboards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  no-storyboards
+//
+//  Created by Emanuel  Guerrero on 2/29/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
